6386b13cfb1c24d9b3208de58377f9283a561ff3
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-04-27 13:27:34 +0200

    fixed an error in the script which caused some problems by copying wrong memory spaces and other calculation errors

--- src/validateChecksum.asm
+++ src/validateChecksum.asm
@@ -16,102 +16,62 @@ validate_checksum:
 	#la	$a0 ibanstr
 	# store address in t7
 	move $t7 $a0
+	addi $a0 $a0 2
 	
-	# Load character at address a0 to s0
-	lb $s0 ($a0)
-	#convert A to 10 by substracting 31 from ascii code
-	addi $s0 $s0 -55
-	# increment address by 1
-	addi $a0 $a0 1
-	
-	# Load second character at a1 to s1
-	lb $s1 ($a0)
-	# convert A to 10 by substracting 31 from ascii code
-	addi $s1 $s1 -55
-	# increment address by 1
-	addi $a0 $a0 1
-	
-	# Load second character at a2 to s1
+	# Load second character at a0 to s6
 	lb $s6 ($a0)
 	addi $s6 $s6 -48
 	# increment address by 1
 	addi $a0 $a0 1
 	
-	# Load second character at a3 to s1
+	# Load second character at a0 to s7
 	lb $s7 ($a0)
 	addi $s7 $s7 -48
 	# increment address by 1
 	addi $a0 $a0 1
 
 
-	# s0..s3 now contains the first 4 bytes
+	# s0 and s1 now contains the two check digits
 	
 	
-	b copyMem
+	b moveMem
 
 
-copyMem:
+moveMem:
 
-	# set t0 to 18 since 18 bytes need to be moved
-	li $t0 18
+	# copy target address
+	move $a0 $t7
+	# increment source address by offset of 4
+	addi $a1 $a0 4
+	li $a2 18
 
-	# copy address from a0 to a1
-	move $a1 $a0
-	
-	# reset address of a0 to first address
-	subi $a0 $a0 4
+	jal memcpy
 
-	# a0 contains the source address for copy
-	# a1 contains the target address for copy
-
-
-	b .loop
-
-.loop:
-	
-	# check if t0 is 0
-	# if so, jump to .done
-	beqz $t0 .done
-	
-	# else move $a1 $a0
-	#move $a0 $a1
-	lb $a2 ($a1)
-	sb $a2 ($a0)
-	
-	# increment a0 by 1
-	addi $a0 $a0 1
-	
-	# increment a1 by 1
-	addi $a1 $a1 1
-	
-	# decrement t0 by 1
-	addi $t0 $t0 -1
-	
-	b .loop
-
-
+	b .done
 .done:
 
+	move $a0 $t7
+	addi $a0 $a0 18
 
-	# sb $s0 ($a0) to store first character as int at addr a0
+	# store 1 as ascii at addr a0
 	li $s0 49
 	sb $s0 ($a0)
 	# increment a0 by 1
 	addi $a0 $a0 1
 	
-	# sb $s1 ($a0) to store second character as int at addr a0
+	# store 3 as ascii at addr a0
 	li $s0 51
 	sb $s0 ($a0)
 	# increment a0 by 1
 	addi $a0 $a0 1
 	
-	# sb $s2 ($a0) to store third character as int at addr a0
+	# store 1 as ascii at addr a0
 	li $s0 49
 	sb $s0 ($a0)
 	# increment a0 by 1
 	addi $a0 $a0 1
 	
-	# sb $s3 ($a0) to store fourth character as int at addr a0
+	# store 4 as ascii at addr a0
 	li $s0 52
 	sb $s0 ($a0)
 	
@@ -134,7 +94,8 @@ copyMem:
 	# set up values for manual mod calculation
 	li $s1 10
 	li $a2 97
-	
+
+
 	# calculate modulo manually of the last two numbers (check digits)
 	mul $s0 $v0 $s1
 	add $s0 $s0 $s6

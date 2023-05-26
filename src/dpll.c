9e08cd0235bb89c1599920621ec430df85a5bc93
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-05-26 16:39:07 +0200

    nearly finished dpll - most working

--- src/dpll.c
+++ src/dpll.c
@@ -1,5 +1,6 @@
 #include "dpll.h"
 
+#include "cnf.h"
 #include "err.h"
 #include "list.h"
 #include "util.h"
@@ -42,6 +43,59 @@ void popAssignment(List* stack) {
 }
 
 /**
+ * Function to negate a truth value
+ */
+TruthValue negateTruthValue(TruthValue val) {
+    return (val == TRUE) ? FALSE : TRUE;
+}
+
+/**
+ * Check if all clauses are satisfied
+ */
+int allSatisfied(CNF* cnf) {
+    // create iterator to iterate over all clauses
+    ListIterator clauseIterator = mkIterator(&cnf->clauses);
+
+    // count number of clauses in stack
+    int numberOfClauses = 0;
+
+    // loop over all clauses in stack
+    while (getCurr(&clauseIterator) != NULL) {
+        // increase number of Clauses
+        numberOfClauses++;
+
+        // move to next clause in stack
+        next(&clauseIterator);
+
+        // check if there is not next clause
+        if (clauseIterator.current == NULL) break;
+    }
+
+    // reset iterator
+    clauseIterator = mkIterator(&cnf->clauses);
+
+    // loop over all clauses in stack by number from before
+    for (int i = 0; i < numberOfClauses; i++) {
+        // get current clause from iterator
+        Clause* c = getCurr(&clauseIterator);
+
+        // check if clause is false
+        if (c->val == FALSE) {
+            // unsatisfied
+            return 0;
+        }
+
+        // move to next clause in stack
+        next(&clauseIterator);
+
+        // exit loop if there is no next clause
+        if (clauseIterator.current == NULL) break;
+    }
+    // satisfied
+    return 1;
+}
+
+/**
  * Führt eine Iteration des DPLL Algorithmus aus.
  *
  * @param vt       die zugrunde liegende Variablentabelle
@@ -62,11 +116,71 @@ void popAssignment(List* stack) {
  *                -1 if the algorithm should terminate with UNSAT
  */
 int iterate(VarTable* vt, List* stack, CNF* cnf) {
-    // TODO Implement me!
-    NOT_IMPLEMENTED;
-    UNUSED(vt);
-    UNUSED(stack);
-    UNUSED(cnf);
+    int terminationCode = 0;
+
+    // counts number of chosen values
+    int numberOfCHOSEN = 0;
+
+    // create VarIndex to hold next variable
+    VarIndex nextVar = 0;
+
+    // create iterator to iterate over stack
+    ListIterator stackIterator = mkIterator(stack);
+
+    Assignment* curAssignment;
+
+    while (terminationCode == 0) {
+        // check if all clauses are satisfied:
+        if (allSatisfied(cnf)) {
+            // abort with Satisfied
+            return 1;
+        }
+        // if one clause is not satisfied
+        else if (!allSatisfied(cnf)) {
+            // check if there is at least one value which was chosen
+            if (numberOfCHOSEN > 0) {
+                // get current assignment from iterator
+                curAssignment = getCurr(&stackIterator);
+
+                // loop until a reason is CHOSEN
+                while (curAssignment->reason == IMPLIED) {
+                    // go back in stack
+                    next(&stackIterator);
+
+                    // remove assignment from stack
+                    popAssignment(stack);
+                    numberOfCHOSEN--;
+                }
+
+                // change reason from CHOSEN to IMPLIED in curAssignment
+                // negate truthValue of variable and store back
+                VarIndex index = curAssignment->var;
+                TruthValue val = getVariableValue(vt, curAssignment->var);
+                curAssignment->reason = IMPLIED;
+                updateVariableValue(vt, index, negateTruthValue(val));
+                numberOfCHOSEN--;
+            } else {
+                // abort with unsatisfied
+                return -1;
+            }
+        }
+
+        // if unit clause exists
+        else if (getNextUndefinedVariable(vt) != 0) {
+            // fulfill next clause by setting its value to true and to implied
+            updateVariableValue(vt, nextVar, TRUE);
+            pushAssignment(stack, nextVar, IMPLIED);
+            numberOfCHOSEN--;
+        }
+
+        // select next free variable and set to true
+        nextVar = getNextUndefinedVariable(vt);
+        updateVariableValue(vt, nextVar, TRUE);
+        // set variable as CHOSEN since it not a unit clause
+        pushAssignment(stack, nextVar, CHOSEN);
+        numberOfCHOSEN++;
+    }
+    return terminationCode;
 }
 
 char isSatisfiable(VarTable* vt, CNF* cnf) {

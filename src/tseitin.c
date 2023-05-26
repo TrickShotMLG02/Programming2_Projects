d2779b5982b4e096d61b92b402d69928781e196d
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2023-05-26 13:32:44 +0200

    file transfer

--- src/tseitin.c
+++ src/tseitin.c
@@ -59,11 +59,103 @@ void addTernaryClause(VarTable* vt, CNF* cnf, Literal a, Literal b, Literal c) {
  * @return     the variable x, as described above
  */
 VarIndex addClauses(VarTable* vt, CNF* cnf, const PropFormula* pf) {
-    // TODO Implement me!
-    NOT_IMPLEMENTED;
-    UNUSED(vt);
-    UNUSED(cnf);
-    UNUSED(pf);
+    // if formula is just a variable (literal) addUnaryClause and return the
+    // varIndex of the variable
+    if (pf->kind == VAR) {
+        // return varIndex of variable
+        return pf->data.var;
+
+    } else if (pf->kind == NOT) {
+        // recursively check sub formula of not and get VarIndex
+        VarIndex op_index = addClauses(vt, cnf, pf->data.single_op);
+
+        // create a fresh variable to store clause in
+        VarIndex i = mkFreshVariable(vt);
+
+        // make new clause with variable
+        mkTernaryClause(vt, op_index, i, 0);
+
+        // negate the VarIndex since negation is represented by negative index
+        // of variable in vartable
+        mkTernaryClause(vt, -op_index, -i, 0);
+
+        // return varIndex of fresh variable
+        return i;
+
+    } else if (pf->kind == IMPLIES) {
+        // implication
+        // (P → Q) ↔ (¬P ∨ Q)
+        //
+
+        // negate first operand and store in f1
+        PropFormula* f1 = mkUnaryFormula(NOT, pf->data.operands[0]);
+        // store second operand in f2
+        PropFormula* f2 = pf->data.operands[1];
+
+        // create new formula for implication alternative
+        PropFormula* impl = mkBinaryFormula(OR, f1, f2);
+
+        // get varIndex for implication transformation
+        VarIndex impl_index = addClauses(vt, cnf, impl);
+
+        // return index of implication transformation
+        return impl_index;
+
+        // make transformation for implication
+    } else if (pf->kind == EQUIV) {
+        // Equivalenz
+        // (P ↔ Q) ↔ (P ^ Q) v (¬P ^ ¬Q)
+        //
+
+        // negate first operand and store in f1
+        PropFormula* f1 = pf->data.operands[0];
+        // store second operand in f2
+        PropFormula* f2 = pf->data.operands[1];
+
+        // create new formula for equiv (left)
+        PropFormula* equiv_l = mkBinaryFormula(AND, f1, f2);
+
+        // negate f1 and f2
+        f1 = mkUnaryFormula(NOT, f1);
+        f2 = mkUnaryFormula(NOT, f2);
+
+        // create new formula for equiv (right)
+        PropFormula* equiv_r = mkBinaryFormula(AND, f1, f2);
+
+        // create new formula for combining both
+
+        PropFormula* equiv = mkBinaryFormula(OR, equiv_l, equiv_r);
+
+        // get varIndex for equiv transformation
+        VarIndex impl_index = addClauses(vt, cnf, equiv);
+
+        // return index of equiv transformation
+        return impl_index;
+
+        // make transformation for equiv
+    } else {
+        // recursively check both operands and get resulting VarIndex
+        VarIndex i1 = addClauses(vt, cnf, pf->data.operands[0]);
+        VarIndex i2 = addClauses(vt, cnf, pf->data.operands[1]);
+
+        // add binary clause with both varIndexes
+        addBinaryClause(vt, cnf, i1, i2);
+
+        // create a fresh variable to store clause in
+        VarIndex i = mkFreshVariable(vt);
+
+        // create ternary clause from binary operand indexes
+        Clause* c = mkTernaryClause(vt, i1, i2, 0);
+
+        // add clause to fresh variable parent clauses
+        addParentClause(vt, i, c);
+
+        // add created clause to cnf
+        addClauseToCNF(cnf, c);
+
+        // return varIndex of fresh variable
+        return i;
+    }
 }
 
 CNF* getCNF(VarTable* vt, const PropFormula* f) {

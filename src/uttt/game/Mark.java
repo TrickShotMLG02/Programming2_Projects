ba992d06acc148485c20e755d34153293fed68af
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2023-06-07 14:10:35 +0200

    implemented Mark class

---
+++ src/uttt/game/Mark.java
@@ -0,0 +1,42 @@
+package uttt.game;
+
+import uttt.utils.Symbol;
+
+public class Mark implements MarkInterface {
+
+    private Symbol symbol;
+    private int position;
+
+    public Mark(Symbol symbol, int position) {
+        setSymbol(symbol);
+        setPosition(position);
+    }
+
+    @Override
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public void setSymbol(Symbol symbol) throws IllegalArgumentException {
+        // check if symbol is null -> set it to Empty, otherwise apply symbol
+        if (symbol == null)
+            symbol = Symbol.EMPTY;
+        else
+            this.symbol = symbol;
+    }
+
+    private void setPosition(int position) {
+        // set position
+        if (position < 0 || position > 8)
+            throw new IllegalArgumentException("Position must be between 0 and 8");
+        else
+            this.position = position;
+    }
+
+}

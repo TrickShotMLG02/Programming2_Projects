aa01d53233e030bc8c9822a6c182c4ff0a7da1a8
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2023-06-07 14:14:24 +0200

    Removed unused import

--- src/uttt/game/Board.java
+++ src/uttt/game/Board.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-import javax.lang.model.util.ElementScanner14;
-
 import uttt.utils.Symbol;
 
 public class Board implements BoardInterface {

7231496c6b01eece47f8507ee31d4fa8dfe60921
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-06-08 11:03:41 +0200

    Restructured Tests and optimized some things

--- src/uttt/tests/MarkInterfaceTest.java
+++ src/uttt/tests/MarkInterfaceTest.java
@@ -20,36 +20,55 @@ public class MarkInterfaceTest {
 	public void setUp() throws Exception {
 		markCross = UTTTFactory.createMark(Symbol.CROSS, 0);
 		markCircle = UTTTFactory.createMark(Symbol.CIRCLE, 1);
-		markInvalid = UTTTFactory.createMark(Symbol.EMPTY, 0);
-		markEmpty = UTTTFactory.createMark(Symbol.EMPTY, 0);
+		markEmpty = UTTTFactory.createMark(Symbol.EMPTY, 4);
+		markInvalid = UTTTFactory.createMark(Symbol.EMPTY, 8);
 	}
 
 	@Test
-	public void simpleSetPieceTest() {
-
-		assertThrows(Exception.class, () -> {
-			markInvalid = UTTTFactory.createMark(Symbol.EMPTY, -1);
-		});
-
-		assertThrows(Exception.class, () -> {
-			markInvalid = UTTTFactory.createMark(Symbol.EMPTY, 10);
-		});
-
+	public void checkForMarksNotNull() {
 		// check if marks are not null
 		assertNotNull(markCross);
 		assertNotNull(markCircle);
+		assertNotNull(markInvalid);
+		assertNotNull(markEmpty);
+	}
 
+	@Test
+	public void invalidIndexOnCreation() {
+		// check for index below bounds
+		assertThrows(Exception.class, () -> {
+			markInvalid = UTTTFactory.createMark(Symbol.EMPTY, -1);
+			markInvalid.getPosition();
+			markInvalid.getSymbol();
+		});
+
+		// check for index above bounds
+		assertThrows(Exception.class, () -> {
+			markInvalid = UTTTFactory.createMark(Symbol.EMPTY, 9);
+			markInvalid.getPosition();
+			markInvalid.getSymbol();
+		});
+	}
+
+	@Test
+	public void invalidSetSymbolTest() {
 		// check if markInvalid throws an exception on setSymbol with null
 		assertThrows(Exception.class, () -> {
 			markInvalid.setSymbol(null);
 		});
+	}
 
+	@Test
+	public void testGetPositions() {
 		// Check if positions are equal to the ones set in setUp()
 		assertEquals(0, markCross.getPosition());
 		assertEquals(1, markCircle.getPosition());
-		assertEquals(0, markEmpty.getPosition());
-		assertEquals(0, markInvalid.getPosition());
+		assertEquals(4, markEmpty.getPosition());
+		assertEquals(8, markInvalid.getPosition());
+	}
 
+	@Test
+	public void testGetSymbols() {
 		// check if symbol names are correct
 		assertEquals("X", markCross.getSymbol().toString());
 		assertEquals("O", markCircle.getSymbol().toString());

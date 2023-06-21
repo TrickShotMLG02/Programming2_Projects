9c25b0de0eed5a4c2d93dbdbf34125232ceace89
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-06-21 18:05:08 +0200

    removed unneccessary import

--- src/raytracer/tests/MyRayTraceTest.java
+++ src/raytracer/tests/MyRayTraceTest.java
@@ -1,22 +1,19 @@
 package raytracer.tests;
 
-import static org.junit.Assert.assertNotNull;
-
 import org.junit.Test;
-
 
 /**
  * Within this class and/or package you can implement your own tests that will
  * be run with the reference implementation.
  *
  * Note that no classes or interfaces will be available, except those initially
- * provided. 
+ * provided.
  */
 public class MyRayTraceTest {
 
 	@Test
 	public void test() {
-		assert(42 == 42);
+		assert (42 == 42);
 	}
 
 }

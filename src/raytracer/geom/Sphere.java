53e85c957ea88ccf551d1950ec5a33175ea5c624
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-06-21 02:08:49 +0200

    fixed Sphere implementation

--- src/raytracer/geom/Sphere.java
+++ src/raytracer/geom/Sphere.java
@@ -11,36 +11,17 @@ import raytracer.math.Vec3;
 public class Sphere extends BBoxedPrimitive {
 
     private final Point m;
-    private final Vec3 u, v, n;
     private final float r;
 
     /**
      * Creates a new Sphere
      * 
-     * @param c  the point of the center of the sphere
-     * @param s1 point 1 which is on the surface of the sphere
-     * @param s2 point 2 which is on the surface of the sphere
+     * @param c the point of the center of the sphere
+     * @param r the radius of the sphere
      */
-    public Sphere(final Point c, final Point s1, final Point s2) {
-        // create bounding box with dimensions of diameter the sphere determined by
-        // center and a point (s1) which is on the surface of the sphere
-        super(BBox.create(c, s1));
-
-        // set radius of sphere by calculating length of vector from center to surface
-        // point
-        r = c.sub(s1).norm();
-
-        // set m to center point
+    public Sphere(final Point c, final float r) {
+        this.r = r;
         this.m = c;
-
-        // set u to vector from center to surface point 1
-        this.u = s1.sub(c);
-
-        // set v to vector from center to surface point 2
-        this.v = s2.sub(c);
-
-        // calculate normal vector of the two vectos pointing from center to surface
-        this.n = u.cross(v).normalized();
     }
 
     @Override
@@ -60,11 +41,11 @@ public class Sphere extends BBoxedPrimitive {
              */
             private Point point = null;
             // r is the distance of the ray
-            private float r;
+            private float d;
 
             @Override
             public float getParameter() {
-                return r;
+                return d;
             }
 
             @Override
@@ -98,7 +79,7 @@ public class Sphere extends BBoxedPrimitive {
                 float discriminant = (float) Math.pow(b, 2) - 4 * a * c;
 
                 // no solution for equation -> no intersection
-                if (discriminant < 0) {
+                if (r <= 0 || discriminant < 0) {
                     return false;
                 } else {
                     // abc-formula
@@ -117,7 +98,7 @@ public class Sphere extends BBoxedPrimitive {
                     // check that hit was good
                     if (solution <= tmax && solution >= tmin) {
                         // store solution as raycast hit distance
-                        r = solution;
+                        d = solution;
 
                         // return true, since hit was valid
                         return true;
@@ -131,7 +112,7 @@ public class Sphere extends BBoxedPrimitive {
             @Override
             public Vec2 getUV() {
                 // TODO: computeSphereUV from impactPoint as vector
-                return Util.computeSphereUV(m.sub(getPoint()));
+                return Util.computeSphereUV(getPoint().sub(m));
             }
 
             @Override
@@ -145,14 +126,14 @@ public class Sphere extends BBoxedPrimitive {
 
     @Override
     public int hashCode() {
-        return m.hashCode() ^ u.hashCode() ^ v.hashCode();
+        return m.hashCode();
     }
 
     @Override
     public boolean equals(final Object other) {
         if (other instanceof Sphere) {
             final Sphere cobj = (Sphere) other;
-            return cobj.m.equals(m) && cobj.u.equals(u) && cobj.v.equals(v);
+            return cobj.m.equals(m) && this.r == r;
         }
         return false;
     }

636d006c4cedf759e98bff5db7e0625561605487
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-06-22 13:58:23 +0200

    fixed all issues with phong shader

--- src/raytracer/shade/Phong.java
+++ src/raytracer/shade/Phong.java
@@ -7,6 +7,7 @@ import raytracer.core.LightSource;
 import raytracer.core.Shader;
 import raytracer.core.Trace;
 import raytracer.math.Color;
+import raytracer.math.Vec3;
 
 public class Phong implements Shader {
 
@@ -38,48 +39,69 @@ public class Phong implements Shader {
     @Override
     public Color shade(Hit hit, Trace trace) {
 
-        // set phong color to ambient color
-        Color phong = ambient;
-
         // get light sources
         List<LightSource> lights = trace.getScene().getLightSources().stream().toList();
 
-        // set base color as black
+        // initialize colors as Black
         Color diffusion = Color.BLACK;
+        Color specularity = Color.BLACK;
+        Color phong = Color.BLACK;
+
+        Color baseColor = inner.shade(hit, trace);
 
         // iterate over all light sources
         for (int i = 0; i < lights.size(); i++) {
             LightSource light = lights.get(i);
 
-            // check if shadow ray hits something on way from hit point to light source
-            // send new ray back
-            Trace shadowTrace = trace.spawn(hit.getPoint(), trace.getRay().invDir().normalized());
-            if (shadowTrace.getHit().hits()) {
+            // calculate vector from hitpoint to light
+            Vec3 lightVec = light.getLocation().sub(hit.getPoint()).normalized();
 
-                // add diffusion for current light to diffusion color
-                diffusion = diffusion.add(light.getColor().mul(inner.shade(hit, trace)).scale(diffuse)
-                        .scale(Math.max(0, hit.getNormal().normalized().dot(trace.getRay().dir().inv().normalized()))));
+            // calculate reflection vector of light on hitpoint from camera position
+            Vec3 reflectionVector = trace.getRay().reflect(hit.getPoint(), hit.getNormal()).dir();
 
+            // create trace which points to light source
+            Trace shadowTrace = trace.spawn(hit.getPoint(), lightVec);
+
+            // check that trace is not blocked by any object
+            if (!shadowTrace.getHit().hits()) {
+
+                // grab color of light source
+                Color light_baseColor = light.getColor().mul(baseColor);
+
+                /*
+                 * calculate color for diffusion
+                 */
+
+                // calculate diffusion coefficient using scalar product n_v
+                float diffusionCoefficient = Math.max(0, hit.getNormal().dot(lightVec));
+
+                // add diffusion for current light source to diffusion color
+                diffusion = diffusion.add(light_baseColor).scale(diffuse).scale(diffusionCoefficient);
+
+                /*
+                 * calculate color for specularity
+                 */
+
+                // calculate angle of reflection vector using scalar product
+                float reflectionAngle = reflectionVector.dot(lightVec);
+
+                // calculate specularity coefficient using the reflectionAngle
+                float specularityCoefficient = (float) Math.pow(Math.max(0, reflectionAngle), smoothness);
+
+                // calculate specularity for current light source
+                specularity = specularity.add(light.getColor().scale(specular).scale(specularityCoefficient));
             }
+
         }
+
+        /*
+         * apply colors to phong
+         */
+
+        phong = phong.add(ambient);
 
         // add diffusion to phong value
         phong = phong.add(diffusion);
-
-        // set base color as black
-        Color specularity = Color.BLACK;
-
-        // iterate over all light sources
-        for (int i = 0; i < lights.size(); i++) {
-            LightSource light = lights.get(i);
-
-            // add specularity for current light to diffusion color
-            specularity = specularity.add(light.getColor().scale(specular)
-                    .scale((float) Math.pow(Math.max(0,
-                            trace.getRay().reflect(hit.getPoint(), hit.getNormal()).dir()
-                                    .dot(trace.getRay().dir().inv())),
-                            smoothness)));
-        }
 
         // add specularity to phong value
         phong = phong.add(specularity);

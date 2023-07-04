91d2b0547780c3dbc6dd622298e14b0efa4e2f51
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-04 22:33:07 +0200

    added util file with numerous usefule functions

--- src/tinycc/implementation/Util.java
+++ src/tinycc/implementation/Util.java
@@ -1,8 +1,16 @@
 package tinycc.implementation;
 
+import java.lang.reflect.Constructor;
+
+import tinycc.parser.Token;
 import tinycc.parser.TokenKind;
 
 public class Util {
+
+    public static final String CREATE_BASE_TYPE = "tinycc.implementation.type.BaseTypes.";
+    public static final String CREATE_BINARY_OPERATOR = "tinycc.implementation.expression.BinaryOperators.";
+    public static final String CREATE_PRIMARY_EXPRESSION = "tinycc.implementation.expression.PrimaryExpressions.";
+
     public static String capitalizeEnumName(String enumName) {
         char[] chrs = enumName.toCharArray();
         String formattedName = "";
@@ -28,15 +36,15 @@ public class Util {
      * @return new Object
      */
     @SuppressWarnings({"unchecked", "rawtypes"})
-    public static <T> T create(TokenKind kind, Class superclassType) {
+    public static <T> T create(TokenKind kind, Class superclassType, String typePackage) {
         // extract text of kind
-        String strKind = kind.getText();
+        String strKind = kind.name().toLowerCase();
 
         // capitalize first letter of string and rest lowercase
         String formattedKind = Util.capitalizeEnumName(strKind);
 
         // the package path of the base types
-        String path = "tinycc.implementation.type.BaseTypes.";
+        String path = typePackage;
 
         // concat package path and kind
         String fullClassName = path + formattedKind;
@@ -49,6 +57,39 @@ public class Util {
             if (superclassType.isAssignableFrom(cls)) {
                 // return new Object of type cls
                 return (T) cls.getDeclaredConstructor().newInstance();
+            }
+        } catch (Exception e) {
+            // TODO: what should i do on invalid input?
+        }
+        return null;
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static <T> T create(Token token, Class superclassType) {
+        // extract text of kind
+        String strKind = token.getKind().name().toLowerCase();
+
+        // capitalize first letter of string and rest lowercase
+        String formattedKind = Util.capitalizeEnumName(strKind);
+
+        // formatted kind contains if it is number or string...
+
+        // the package path of the base types
+        String path = CREATE_PRIMARY_EXPRESSION;
+
+        // concat package path and kind
+        String fullClassName = path + formattedKind;
+        
+        try {
+            // create class with name from kind
+            Class<?> cls = Class.forName(fullClassName);
+
+            // check if cls is a sub class of superClassType (since only then typecasting should be possible)
+            if (superclassType.isAssignableFrom(cls)) {
+                // grab constructor since we need to pass a parameter to it
+                Constructor<T> constructor = (Constructor<T>) cls.getDeclaredConstructor(token.getClass());
+                // call constructor with parameter
+                return constructor.newInstance(token);
             }
         } catch (Exception e) {
             // TODO: what should i do on invalid input?

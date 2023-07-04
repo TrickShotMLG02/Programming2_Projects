5340625d89e3e75abbe8278decfb9656ff33a41e
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-04 22:39:43 +0200

    added toString to BinaryOperator

--- src/tinycc/implementation/expression/BinaryOperator.java
+++ src/tinycc/implementation/expression/BinaryOperator.java
@@ -1,6 +1,14 @@
 package tinycc.implementation.expression;
 
-public class BinaryOperator {
+import tinycc.parser.TokenKind;
 
-    
+public class BinaryOperator {
+    @Override
+    public String toString() {
+        // grab class name and convert it to Enum Naming Convention
+        String enumElemName = this.getClass().getSimpleName().toUpperCase();
+        // grab element from enum with name
+        TokenKind enumElem = TokenKind.valueOf(enumElemName);
+        return enumElem.getText();
+    }   
 }

af2dff79a08d10f8f5893f3e99e4c73affb06a08
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-05 14:24:27 +0200

    implemented ExpressionStatement

--- src/tinycc/implementation/statement/Statements/ExpressionStatement.java
+++ src/tinycc/implementation/statement/Statements/ExpressionStatement.java
@@ -1,15 +1,24 @@
 package tinycc.implementation.statement.Statements;
 
+import tinycc.diagnostic.Locatable;
+import tinycc.implementation.expression.Expression;
 import tinycc.implementation.statement.Statement;
 
 public class ExpressionStatement extends Statement {
+
+    Locatable loc;
+    Expression expression;
+
+    public ExpressionStatement(Locatable loc, Expression expression) {
+        this.loc = loc;
+        this.expression = expression;
+    }
 
     // TODO: Implement ExpressionStatement
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        return expression.toString();
     }
     
 }

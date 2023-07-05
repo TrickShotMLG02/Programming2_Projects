e0f39ce35c0aa661fba2eab242c2d085f4f1aa5c
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-05 16:44:21 +0200

    Implemented Function TopLevelConstruct

--- src/tinycc/implementation/TopLevelConstructs/ExternalDeclarations/Function.java
+++ src/tinycc/implementation/TopLevelConstructs/ExternalDeclarations/Function.java
@@ -1,13 +1,26 @@
 package tinycc.implementation.TopLevelConstructs.ExternalDeclarations;
 
+import java.util.List;
+
 import tinycc.implementation.TopLevelConstructs.ExternalDeclaration;
+import tinycc.implementation.statement.Statement;
 import tinycc.implementation.type.Type;
 import tinycc.parser.Token;
 
 public class Function extends ExternalDeclaration {
+    private List<Token> parameterNames;
+    private Statement body;
 
-    public Function(Type type, Token name) {
+    public Function(Type type, Token name, List<Token> parameterNames, Statement body) {
         super(type, name, null);
+        this.parameterNames = parameterNames;
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        // TODO: Implement toString method
+        return getType().toString();
     }
     
 }

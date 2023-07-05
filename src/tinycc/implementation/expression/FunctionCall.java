1a1d013b46fcdd13f45222a4fe288fad966557fb
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-05 10:38:27 +0200

    implemented FunctionCall

--- src/tinycc/implementation/expression/FunctionCall.java
+++ src/tinycc/implementation/expression/FunctionCall.java
@@ -1,11 +1,30 @@
 package tinycc.implementation.expression;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import tinycc.parser.Token;
+
 public class FunctionCall extends Expression {
+
+    Token token;
+    Expression callee;
+    List<Expression> arguments;
+    
+    public FunctionCall(Token token, Expression callee, List<Expression> arguments) {
+        this.token = token;
+        this.callee = callee;
+        this.arguments = arguments;
+    }
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        List<Expression> tmpExp = new ArrayList<>();
+        tmpExp.add(callee);
+        tmpExp.addAll(arguments);
+
+        return "Call" + Arrays.toString(tmpExp.toArray());
     }
     
 }

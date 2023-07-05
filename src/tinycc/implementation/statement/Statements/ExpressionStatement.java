094d3a3073f195d999e4d94c5321a89892045145
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-05 10:45:58 +0200

    created classes for ExpressionStatement and While, but need to implement them

---
+++ src/tinycc/implementation/statement/Statements/ExpressionStatement.java
@@ -0,0 +1,15 @@
+package tinycc.implementation.statement.Statements;
+
+import tinycc.implementation.statement.Statement;
+
+public class ExpressionStatement extends Statement {
+
+    // TODO: Implement ExpressionStatement
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+    
+}
---
+++ src/tinycc/implementation/statement/Statements/While.java
@@ -0,0 +1,15 @@
+package tinycc.implementation.statement.Statements;
+
+import tinycc.implementation.statement.Statement;
+
+public class While extends Statement{
+
+    // TODO: Implement WhileStatement
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+    
+}

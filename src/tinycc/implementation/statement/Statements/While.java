089d88aeb25cce77daa3e93216dcaaf68671ec34
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-05 16:08:25 +0200

    implemented While

--- src/tinycc/implementation/statement/Statements/While.java
+++ src/tinycc/implementation/statement/Statements/While.java
@@ -1,15 +1,25 @@
 package tinycc.implementation.statement.Statements;
 
+import tinycc.diagnostic.Locatable;
+import tinycc.implementation.expression.Expression;
 import tinycc.implementation.statement.Statement;
 
 public class While extends Statement{
 
-    // TODO: Implement WhileStatement
+    Locatable loc;
+    Expression condition;
+    Statement body;
+
+    public While(Locatable loc, Expression condition, Statement body) {
+        this.loc = loc;
+        this.condition = condition;
+        this.body = body;
+    }
+
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        return "While[" + condition + "," + body + "]";
     }
     
 }

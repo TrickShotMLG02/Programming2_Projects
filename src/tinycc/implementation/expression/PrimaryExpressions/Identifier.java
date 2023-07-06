f7f1060a97a8b5f31dd0c84ed217ea1dd3ce5ddd
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-06 21:22:34 +0200

    implemented checkType function

--- src/tinycc/implementation/expression/PrimaryExpressions/Identifier.java
+++ src/tinycc/implementation/expression/PrimaryExpressions/Identifier.java
@@ -1,6 +1,11 @@
 package tinycc.implementation.expression.PrimaryExpressions;
 
+import tinycc.diagnostic.Diagnostic;
+import tinycc.implementation.Scope;
+import tinycc.implementation.Util;
 import tinycc.implementation.expression.PrimaryExpression;
+import tinycc.implementation.statement.Statements.Declaration;
+import tinycc.implementation.type.Type;
 import tinycc.parser.Token;
 
 public class Identifier extends PrimaryExpression {
@@ -12,5 +17,20 @@ public class Identifier extends PrimaryExpression {
     @Override
     public java.lang.String toString() {
         return "Var_" + getToken().getText();
+    }
+
+    @Override
+    public Type checkType(Diagnostic d, Scope s) {
+        // check if token was declared earlier in scope
+        try {
+            // grab declaration from scope
+            Declaration decl = s.lookup(getToken().getInputName());
+
+            // return the type of the declaration
+            return Util.createType(getToken().getKind());
+        } catch (Exception e) {
+            d.printError(getToken(), "", null);
+            return null;
+        }
     }
 }

9b9fbb101788821bf0448cd2befdd5685306e2a5
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-06 21:36:10 +0200

    overwrote checkType function

--- src/tinycc/implementation/expression/PrimaryExpressions/String.java
+++ src/tinycc/implementation/expression/PrimaryExpressions/String.java
@@ -1,6 +1,9 @@
 package tinycc.implementation.expression.PrimaryExpressions;
 
+import tinycc.diagnostic.Diagnostic;
+import tinycc.implementation.Scope;
 import tinycc.implementation.expression.PrimaryExpression;
+import tinycc.implementation.type.Type;
 import tinycc.parser.Token;
 
 public class String extends PrimaryExpression {
@@ -12,5 +15,11 @@ public class String extends PrimaryExpression {
     @Override
     public java.lang.String toString() {
         return super.toString() + "\"" + getToken().getText() + "\"";
+    }
+
+    @Override
+    public Type checkType(Diagnostic d, Scope s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkType'");
     }
 }

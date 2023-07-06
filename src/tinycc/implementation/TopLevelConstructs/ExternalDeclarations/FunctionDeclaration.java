66d7344e14d7a5291cc12b0a9f8f298a3548feda
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-06 20:56:16 +0200

    added booleans to determine type, added new constructor

--- src/tinycc/implementation/TopLevelConstructs/ExternalDeclarations/FunctionDeclaration.java
+++ src/tinycc/implementation/TopLevelConstructs/ExternalDeclarations/FunctionDeclaration.java
@@ -1,13 +1,32 @@
 package tinycc.implementation.TopLevelConstructs.ExternalDeclarations;
 
 import tinycc.implementation.TopLevelConstructs.ExternalDeclaration;
+import tinycc.implementation.expression.Expression;
 import tinycc.implementation.type.Type;
 import tinycc.parser.Token;
 
 public class FunctionDeclaration extends ExternalDeclaration {
 
     public FunctionDeclaration(Type type, Token name) {
-        super(type, name, null);
+        this(type, name, null);
+    }
+
+    public FunctionDeclaration(Type type, Token name, Expression init) {
+        super(type, name, init);
     }
     
+    @Override
+    public boolean isFunction() {
+        return false;
+    }
+
+    @Override
+    public boolean isFunctionDeclaration() {
+        return true;
+    }
+
+    @Override
+    public boolean isGlobalVariable() {
+        return false;
+    }
 }
--- src/tinycc/implementation/TopLevelConstructs/ExternalDeclarations/GlobalVariable.java
+++ src/tinycc/implementation/TopLevelConstructs/ExternalDeclarations/GlobalVariable.java
@@ -1,13 +1,32 @@
 package tinycc.implementation.TopLevelConstructs.ExternalDeclarations;
 
 import tinycc.implementation.TopLevelConstructs.ExternalDeclaration;
+import tinycc.implementation.expression.Expression;
 import tinycc.implementation.type.Type;
 import tinycc.parser.Token;
 
 public class GlobalVariable extends ExternalDeclaration {
 
     public GlobalVariable(Type type, Token name) {
-        super(type, name, null);
+        this(type, name, null);
+    }
+
+    public GlobalVariable(Type type, Token name, Expression init) {
+        super(type, name, init);
     }
     
+    @Override
+    public boolean isFunction() {
+        return false;
+    }
+
+    @Override
+    public boolean isFunctionDeclaration() {
+        return false;
+    }
+
+    @Override
+    public boolean isGlobalVariable() {
+        return true;
+    }
 }

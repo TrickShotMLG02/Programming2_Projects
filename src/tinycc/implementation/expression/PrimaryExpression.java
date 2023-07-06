88f85b654771776b79b9c496fe8540e65f22a269
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-06 21:22:19 +0200

    removed default implementation of checkType in abstract class

--- src/tinycc/implementation/expression/PrimaryExpression.java
+++ src/tinycc/implementation/expression/PrimaryExpression.java
@@ -1,10 +1,5 @@
 package tinycc.implementation.expression;
 
-import tinycc.diagnostic.Diagnostic;
-import tinycc.implementation.Scope;
-import tinycc.implementation.Util;
-import tinycc.implementation.statement.Statements.Declaration;
-import tinycc.implementation.type.Type;
 import tinycc.parser.Token;
 
 public abstract class PrimaryExpression extends Expression {
@@ -22,20 +17,6 @@ public abstract class PrimaryExpression extends Expression {
 
     public Token getToken() {
         return token;
-    }
-
-    @Override
-    public Type checkType(Diagnostic d, Scope s) {
-        // lookup token in scope to check if it was declared previously
-        // TODO: make sure this works
-        try {
-            Declaration decl = s.lookup(token.getInputName());
-            return Util.createType(token.getKind());
-        } catch (Exception e) {
-            d.printError(token, "", null);
-            return null;
-        }
-
     }
     
 }

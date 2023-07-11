71c8e1848d239686fff34fb3902402d6c1efa022
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-11 19:22:15 +0200

    reorganized complete class and functions since last approach didn't work well

--- src/tinycc/implementation/CompilationScope.java
+++ src/tinycc/implementation/CompilationScope.java
@@ -1,21 +1,26 @@
 package tinycc.implementation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import tinycc.mipsasmgen.GPRegister;
 
 public class CompilationScope {
+    private final List<GPRegister> unusedTempRegisters;
+    private final List<GPRegister> unusedFunctionRegisters;
+
     private final Map<String, GPRegister> table;
     private final CompilationScope parent;
 
-     /**
+    // constructors
+    /**
      * Create a new Scope with no parent (the first scope)
      */
     public CompilationScope() {
         this(null);
     }
-
     /**
      * Only for internal use of newNestedScope and Scope
      * @param parent
@@ -23,8 +28,10 @@ public class CompilationScope {
     private CompilationScope(CompilationScope parent) {
         this.parent = parent;
         this.table  = new HashMap<String, GPRegister>();
-    }
 
+        unusedTempRegisters = populateUnusedTempRegisters();
+        unusedFunctionRegisters = populateUnusedFunctionRegisters();
+    }
     /**
      * Create a new nested scope, which lies within the current Scope
      * @return a new nested scope
@@ -33,83 +40,101 @@ public class CompilationScope {
         return new CompilationScope(this);
     }
 
-    /**
-     * Add a new Identifier and its respective GPRegister to the current scope
-     * @param id                    The identifier to add
-     * @param d                     The declaration to add
-     * @throws IdAlreadyDeclared    If the identifier is already declared in this scope
-     */
-    public void add(String id, GPRegister d) throws IdAlreadyDeclared {
-        if (table.containsKey(id)) {
-            // update already existing value
-            table.put(id, d);
-            throw new IdAlreadyDeclared(id);
-        }
-        table.put(id, d);
+    // functions to check if register (opt. for variable name) is already in use
+    public boolean isUsed(GPRegister reg) {
+        return table.containsValue(reg);
     }
-
-    /**
-     * Removes a specific identifier and its register from the map
-     * @param id
-     * @throws IdUndeclared
-     */
-    public void remove(String id) throws IdUndeclared {
-        // check if identifier is declared
-        if (table.containsKey(id)) {
-            // remove identifier from table
-            table.remove(id);
-        }
-        else {
-            throw new IdUndeclared(id);
-        }
-    }
-
-    public boolean inUse(String id) {
+    public boolean isUsed(String id) {
         return table.containsKey(id);
     }
 
+    // function to add register to table
+    public void add(String id, GPRegister reg) throws IdAlreadyDeclared {
+        if (!isUsed(id) && !isUsed(reg)) {
+            unusedTempRegisters.remove(reg);
+            unusedFunctionRegisters.remove(reg);
+            table.put(id, reg);
+        }
+        else {
+            throw new IdAlreadyDeclared("register " + reg.name()+ " for variable " + id + " already declared");
+        }
+    }
+    public void add(GPRegister reg) throws IdAlreadyDeclared {
+        add(reg.name(), reg);
+    }
+
+    // function to remove register from table
+    public void remove(String id) throws IdUndeclared {
+        if (isUsed(id)) {
+            // set register for id as unused
+            freeRegister(table.get(id));
+            // remove register for id from table
+            table.remove(id);
+        }
+    }
+    public void remove(GPRegister reg) throws IdUndeclared {
+        if (isUsed(reg)) {
+            // set register reg as unused
+            freeRegister(reg);
+            // remove register for regname from table
+            table.remove(reg.name());
+        }
+        else {
+            throw new IdUndeclared("Register not in use");
+        }
+    }
+
+    // function to get next free register
     public GPRegister getNextFreeTempRegister() {
-        for (int i = 0; i < 10; i++) {
-            GPRegister tmpReg = GPRegister.valueOf("$t" + i);
-
-            // check if register is not in use
-            if (!inUse(tmpReg.name())) {
-                return tmpReg;
-            }  
-        }
-
-        return null;
+        return unusedTempRegisters.get(0);
+    }
+    public GPRegister getNextFreeFunctionRegister() {
+        return unusedFunctionRegisters.get(0);
     }
 
-    /**
-     * Lookup an identifier in all parent scopes.
-     * @param id                The Identifier to look up
-     * @return                  The respective GPRegister of an Identifier
-     * @throws IdUndeclared     If Identifier was not found
-     */
-    public GPRegister lookup(String id) throws IdUndeclared {
+    // functions for poulating lists
+    private List<GPRegister> populateUnusedTempRegisters() {
+        List<GPRegister> regs = new ArrayList<GPRegister>();
 
-        // check if Identifier is already declared in current scope
-        if (table.containsKey(id)) {
-            return table.get(id);
+        // T regs
+        for(int i = 0; i < 10; i++) {
+            regs.add(GPRegister.valueOf("T" + i));
         }
 
-        // otherwhise check in parent scope if parent scope is not null
-        if (parent != null) {
-            return parent.lookup(id);
+        // S regs
+        for(int i = 0; i < 8; i++) {
+            regs.add(GPRegister.valueOf("S" + i));
         }
 
-        // no parent -> thus identifier was not found
-        throw new IdUndeclared(id);
+        return regs;
+    }
+    private List<GPRegister> populateUnusedFunctionRegisters() {
+        List<GPRegister> regs = new ArrayList<GPRegister>();
+
+        // A regs
+        for(int i = 0; i < 4; i++) {
+            regs.add(GPRegister.valueOf("A" + i));
+        }
+
+        return regs;
     }
 
+    // function to free register
+    private void freeRegister(GPRegister reg) {
+        if (populateUnusedFunctionRegisters().contains(reg)) {
+            unusedFunctionRegisters.add(reg);
+        }
+        else if (populateUnusedTempRegisters().contains(reg)) {
+            unusedTempRegisters.add(reg);
+        }
+    }
 
     /*
     * Exceptions for Scopes
     */
     class IdAlreadyDeclared extends Exception {
-        public IdAlreadyDeclared(String id) {
-            super("Identifier "  + id + " already declared in current scope");
+        public IdAlreadyDeclared(String message) {
+            super(message);
         }
     }
     class IdUndeclared extends Exception {

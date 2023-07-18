3e0d66dca755367a55013148872e7dc10e8c67b4
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-18 22:28:04 +0200

    implemented generateCode for SizeOf

--- src/tinycc/implementation/expression/UnaryExpressions/SizeOfExpression.java
+++ src/tinycc/implementation/expression/UnaryExpressions/SizeOfExpression.java
@@ -11,11 +11,13 @@ import tinycc.implementation.type.Type;
 import tinycc.implementation.type.BaseTypes.Char;
 import tinycc.implementation.type.BaseTypes.Int;
 import tinycc.mipsasmgen.GPRegister;
+import tinycc.mipsasmgen.ImmediateInstruction;
 import tinycc.mipsasmgen.MipsAsmGen;
 import tinycc.parser.Token;
 
 public class SizeOfExpression extends UnaryExpression {
 
+    private Type expType = null;
     public SizeOfExpression(Token token, UnaryOperator operator, Expression exp) {
         super(token, operator, exp);
     }
@@ -24,6 +26,7 @@ public class SizeOfExpression extends UnaryExpression {
     public Type checkType(Diagnostic d, Scope s) {
         // grab the type of the expression
         Type applicableType = getExpression().checkType(d, s);
+        expType = applicableType;
 
         // check if the type is a Object Type and complete
         if (applicableType.isObjectType() && applicableType.isComplete()) {
@@ -62,6 +65,15 @@ public class SizeOfExpression extends UnaryExpression {
 
     @Override
     public GPRegister generateCode(CompilationScope s, MipsAsmGen gen) {
-        throw new UnsupportedOperationException("Unimplemented method 'generateCode'");
+        GPRegister valReg = s.getNextFreeTempRegister();
+        int size = -1;
+
+        if (expType != null) {
+            size = expType.getSize();
+        }
+
+        gen.emitInstruction(ImmediateInstruction.ADDI, valReg, GPRegister.ZERO, size);
+
+        return valReg;
     }
 }

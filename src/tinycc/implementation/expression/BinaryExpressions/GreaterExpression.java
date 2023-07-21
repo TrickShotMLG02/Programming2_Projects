88dfc1903cb9186c126edff59a3a23f0d50e14b9
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-21 14:27:27 +0200

    implemented GreaterExpression and LessEqualExpression

--- src/tinycc/implementation/expression/BinaryExpressions/GreaterExpression.java
+++ src/tinycc/implementation/expression/BinaryExpressions/GreaterExpression.java
@@ -7,8 +7,13 @@ import tinycc.implementation.expression.BinaryExpression;
 import tinycc.implementation.expression.BinaryOperator;
 import tinycc.implementation.expression.Expression;
 import tinycc.implementation.type.BaseTypes.Int;
+import tinycc.mipsasmgen.BranchInstruction;
 import tinycc.mipsasmgen.GPRegister;
+import tinycc.mipsasmgen.ImmediateInstruction;
+import tinycc.mipsasmgen.JumpInstruction;
 import tinycc.mipsasmgen.MipsAsmGen;
+import tinycc.mipsasmgen.RegisterInstruction;
+import tinycc.mipsasmgen.TextLabel;
 import tinycc.implementation.type.Type;
 import tinycc.parser.Token;
 
@@ -42,6 +47,45 @@ public class GreaterExpression extends BinaryExpression {
 
     @Override
     public GPRegister generateCode(CompilationScope s, MipsAsmGen gen) {
-        throw new UnsupportedOperationException("Unimplemented method 'generateCode'");
+        GPRegister leftReg = getLeft().generateCode(s, gen);
+        GPRegister rightReg = getRight().generateCode(s, gen);
+
+        GPRegister compResult1 = s.getNextFreeTempRegister();
+        GPRegister compResult2 = s.getNextFreeTempRegister();
+
+        // check if left is greater equal than right
+        // and 
+        // left is not equal to right
+
+        // check if left is greater equal right -> store result in compResult1
+        gen.emitInstruction(RegisterInstruction.SLT, compResult1, rightReg, leftReg);
+
+        // check if left unequal right and store result in compResult2
+        TextLabel lblTrue = gen.makeUniqueTextLabel("_GREATER_EXPRESSION_UNEQUAL");
+        TextLabel lblFalse = gen.makeUniqueTextLabel("_GREATER_EXPRESSION_EQUAL");
+        TextLabel lblExit = gen.makeUniqueTextLabel("_GREATER_EXPRESSION_EXIT");
+
+        gen.emitInstruction(BranchInstruction.BNE, leftReg, rightReg, lblTrue);
+        gen.emitInstruction(JumpInstruction.J, lblFalse);
+
+        gen.emitLabel(lblTrue);
+        gen.emitInstruction(ImmediateInstruction.ADDI, compResult2, GPRegister.ZERO, 1);
+        gen.emitInstruction(JumpInstruction.J, lblExit);
+
+        gen.emitLabel(lblFalse);
+        gen.emitInstruction(ImmediateInstruction.ADDI, compResult2, GPRegister.ZERO, 0);
+        gen.emitInstruction(JumpInstruction.J, lblExit);
+
+        // return compResult1 and compResult2
+        gen.emitLabel(lblExit);
+        gen.emitInstruction(RegisterInstruction.AND, compResult1, compResult1, compResult2);
+
+        try {
+            // free up reg 2
+            s.remove(compResult2);
+        } catch (Exception e) {
+        }
+
+        return compResult1;
     } 
 }
--- src/tinycc/implementation/expression/BinaryExpressions/LessEqualExpression.java
+++ src/tinycc/implementation/expression/BinaryExpressions/LessEqualExpression.java
@@ -8,8 +8,13 @@ import tinycc.implementation.expression.BinaryOperator;
 import tinycc.implementation.expression.Expression;
 import tinycc.implementation.type.Type;
 import tinycc.implementation.type.BaseTypes.Int;
+import tinycc.mipsasmgen.BranchInstruction;
 import tinycc.mipsasmgen.GPRegister;
+import tinycc.mipsasmgen.ImmediateInstruction;
+import tinycc.mipsasmgen.JumpInstruction;
 import tinycc.mipsasmgen.MipsAsmGen;
+import tinycc.mipsasmgen.RegisterInstruction;
+import tinycc.mipsasmgen.TextLabel;
 import tinycc.parser.Token;
 
 public class LessEqualExpression extends BinaryExpression {
@@ -42,6 +47,45 @@ public class LessEqualExpression extends BinaryExpression {
 
     @Override
     public GPRegister generateCode(CompilationScope s, MipsAsmGen gen) {
-        throw new UnsupportedOperationException("Unimplemented method 'generateCode'");
+        GPRegister leftReg = getLeft().generateCode(s, gen);
+        GPRegister rightReg = getRight().generateCode(s, gen);
+
+        GPRegister compResult1 = s.getNextFreeTempRegister();
+        GPRegister compResult2 = s.getNextFreeTempRegister();
+
+        // check if left is less than right -> 
+        // or
+        // left is equal to right
+
+        // check if left is less than right -> store result in compResult1
+        gen.emitInstruction(RegisterInstruction.SLT, compResult1, leftReg, rightReg);
+
+        // check if left unequal right and store result in compResult2
+        TextLabel lblTrue = gen.makeUniqueTextLabel("_LESS_EQUAL_EXPRESSION_UNEQUAL");
+        TextLabel lblFalse = gen.makeUniqueTextLabel("_LESS_EQUAL_EXPRESSION_EQUAL");
+        TextLabel lblExit = gen.makeUniqueTextLabel("_LESS_EQUAL_EXPRESSION_EXIT");
+
+        gen.emitInstruction(BranchInstruction.BEQ, leftReg, rightReg, lblTrue);
+        gen.emitInstruction(JumpInstruction.J, lblFalse);
+
+        gen.emitLabel(lblTrue);
+        gen.emitInstruction(ImmediateInstruction.ADDI, compResult2, GPRegister.ZERO, 1);
+        gen.emitInstruction(JumpInstruction.J, lblExit);
+
+        gen.emitLabel(lblFalse);
+        gen.emitInstruction(ImmediateInstruction.ADDI, compResult2, GPRegister.ZERO, 0);
+        gen.emitInstruction(JumpInstruction.J, lblExit);
+
+        // return compResult1 and compResult2
+        gen.emitLabel(lblExit);
+        gen.emitInstruction(RegisterInstruction.OR, compResult1, compResult1, compResult2);
+
+        try {
+            // free up reg 2
+            s.remove(compResult2);
+        } catch (Exception e) {
+        }
+
+        return compResult1;
     } 
 }

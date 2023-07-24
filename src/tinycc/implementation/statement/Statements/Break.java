02669abeef315bacc2d95167fb28e74f925fbdcc
Tim Schlachter <tisc00020@stud.uni-saarland.de>
2023-07-24 09:18:20 +0200

    added break and continue classes

---
+++ src/tinycc/implementation/statement/Statements/Break.java
@@ -0,0 +1,37 @@
+package tinycc.implementation.statement.Statements;
+
+import tinycc.diagnostic.Diagnostic;
+import tinycc.diagnostic.Locatable;
+import tinycc.implementation.CompilationScope;
+import tinycc.implementation.Scope;
+import tinycc.implementation.TopLevelConstructs.ExternalDeclaration;
+import tinycc.implementation.statement.Statement;
+import tinycc.mipsasmgen.MipsAsmGen;
+
+public class Break extends Statement {
+
+    Locatable loc;
+
+    public Break(Locatable loc) {
+        this.loc = loc;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+
+    @Override
+    public void checkType(Diagnostic d, Scope s, ExternalDeclaration f) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkType'");
+    }
+
+    @Override
+    public void generateCode(CompilationScope s, MipsAsmGen gen) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateCode'");
+    }
+    
+}
